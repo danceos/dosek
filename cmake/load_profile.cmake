d73ac57fb91c35196543e35adf9a10cad62fc6a6
Martin Hoffmann <hoffmann@cs.fau.de>
2013-09-02 16:32:07 +0200

    Using i386 cross compiler from /proj/i4ezs

--- cmake/load_profile.cmake
+++ cmake/load_profile.cmake
@@ -13,18 +13,13 @@ FUNCTION(LOAD_PROFILE ARCH PLATFORM)
 	set(PLATFORM_LAYOUT "${ARCHPATH}/${PLATFORM}/layout.ld")
 
 
-	## setup compiler flags
-	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -nostdlib -fno-builtin")
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_C_FLAGS} ")# -fno-exception")
-
-
 	message(STATUS "Architecture sources: ${ARCH_SRCS}, Platform specifics: ${PLATFORM_SRCS}")
 	message(STATUS "Memory layout: ${PLATFORM_LAYOUT}")
 	add_executable(${ELFFILE} ${PLATFORM_SRCS} ${ARCH_SRCS})
 #target_link_libraries(${ELFFILE} TODO)
 
 	set_target_properties(${ELFFILE} PROPERTIES LINK_FLAGS
-			"-Wl,-T ${PLATFORM_LAYOUT} -Wl,-nostdlib -nostdlib -ffreestanding -O2" )
+			"-Wl,-T -m32 ${PLATFORM_LAYOUT} -Wl,-nostdlib -nostdlib -ffreestanding -O2" )
 
   # Platform specific custom targets/commands.
 	include(${ARCHPATH}/${PLATFORM}/targets.cmake)
