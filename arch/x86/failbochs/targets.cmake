5c9cc043756bf480d22a960dcaef61d4d2d75544
Martin Hoffmann <hoffmann@cs.fau.de>
2013-09-02 16:01:32 +0200

    Added initial boot and build environment for x86.
    Multiboot header is not found yet. :(

---
+++ arch/x86/failbochs/targets.cmake
@@ -0,0 +1,34 @@
+
+#set(GRUB_MKRESCUE "grub-mkrescue")
+find_program(GRUB_MKRESCUE "grub-mkrescue")
+if(GRUB_MKRESCUE)
+		set(ISODIR "${PROJECT_BINARY_DIR}/grub_iso")
+
+		set(ISOFILE "${ELFFILE}.iso")
+
+		message(STATUS "Building bootable ISO for ${ELFFILE} in ${ISODIR}")
+
+		# Generate build directory for grub-mkrescue
+		set(BOOTDIR "${ISODIR}/boot/")
+		set(GRUBDIR "${BOOTDIR}/grub")
+
+		file(MAKE_DIRECTORY ${GRUBDIR})
+		# Copy and configure grub configuration file
+		configure_file("arch/x86/failbochs/grub.cfg.in" "${GRUBDIR}/grub.cfg")
+
+		add_custom_command(
+			SOURCE ${ELFFILE}
+			COMMAND ${CMAKE_COMMAND} -E copy ${ELFFILE} ${BOOTDIR}
+			COMMAND ${GRUB_MKRESCUE}
+			ARGS -o ${ISOFILE} ${ISODIR}
+			OUTPUT ${ISOFILE} ${BOOTDIR}/${ELFFILE}
+			COMMAND qemu-system-i386 -cdrom ${ISOFILE}
+		)
+
+		add_custom_target(run
+				DEPENDS ${ELFFILE} ${ISOFILE}
+				COMMAND echo "Running ${ISOFILE}..."
+		)
+else()
+		message(FATAL_ERROR "grub-mkrescue not found, cannot create bootable iso :(")
+endif()
---
+++ cmake/load_profile.cmake
@@ -0,0 +1,31 @@
+FUNCTION(LOAD_PROFILE ARCH PLATFORM)
+	# Obtain sources
+	set(ARCHPATH "${PROJECT_SOURCE_DIR}/arch/${ARCH}")
+	file(GLOB ARCH_SRCS "${ARCHPATH}/*.cc" "${ARCHPATH}/*.S")
+	file(GLOB PLATFORM_SRCS "${ARCHPATH}/${PLATFORM}/*.cc")
+
+	# Load platform specific custom targets, if present
+
+	# Load flags
+	include("${ARCHPATH}/cmake/flags.cmake")
+
+	# now export our output variables
+	set(PLATFORM_LAYOUT "${ARCHPATH}/${PLATFORM}/layout.ld")
+
+
+	## setup compiler flags
+	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -nostdlib -fno-builtin")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_C_FLAGS} ")# -fno-exception")
+
+
+	message(STATUS "Architecture sources: ${ARCH_SRCS}, Platform specifics: ${PLATFORM_SRCS}")
+	message(STATUS "Memory layout: ${PLATFORM_LAYOUT}")
+	add_executable(${ELFFILE} ${PLATFORM_SRCS} ${ARCH_SRCS})
+#target_link_libraries(${ELFFILE} TODO)
+
+	set_target_properties(${ELFFILE} PROPERTIES LINK_FLAGS
+			"-Wl,-T ${PLATFORM_LAYOUT} -Wl,-nostdlib -nostdlib -ffreestanding -O2" )
+
+  # Platform specific custom targets/commands.
+	include(${ARCHPATH}/${PLATFORM}/targets.cmake)
+ENDFUNCTION(LOAD_PROFILE)
