45fe43259cf4e7c1a6e830038fe2762984809f5f
Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
2014-02-03 15:29:31 +0100

    app/complex1: running testcases
    The three complex testcases are also compiled and run now. The complex
    testcases involve an alarm and an isr.
    
    Change-Id: I37317ce3517480dca701890df1d43d77d6980041

--- app/bcc1/complex1/a.cc
+++ app/bcc1/complex1/a.cc
@@ -9,25 +9,61 @@
  * @brief Just a simple test application
  */
 #include "os.h"
+#include "test/test.h"
+#include "../trace.h"
+#include "syscall.h"
 
 DeclareTask(H1);
 DeclareTask(H2);
 DeclareTask(H3);
 
-int a;
+void test(void) {
+	test_start();
+	StartOS(0);
+}
+
+// new syscall to trigger interrupt using local APIC
+// for now, any function can be called using syscall(),
+// so it is easy to add this for this test.
+noinline void __OS_trigger_syscall(uint8_t irq) {
+	Machine::trigger_interrupt(irq);
+}
+
+static int cycle_count;
 
 TASK(H1) {
+	Trace('1');
 	TerminateTask();
 }
 
 TASK(H2) {
+	Trace('2');
+    arch::syscall(__OS_trigger_syscall, 37, true);
+	Trace('_');
 	TerminateTask();
 }
 
 TASK(H3) {
+	Trace('3');
 	TerminateTask();
 }
 
 ISR2(ISR1) {
+	Trace('.');
 	ActivateTask(H1);
+	Trace(':');
 }
+
+
+
+PreIdleHook() {
+	/* The testcase has finished, check the output */
+	cycle_count++;
+
+	if (cycle_count > 3) {
+		test_start_check();
+		TraceAssert((char *)"2.:1_2.:1_2.:1_");
+		ShutdownMachine();
+	}
+}
+
--- app/bcc1/complex1/c.cc
+++ app/bcc1/complex1/c.cc
@@ -9,26 +9,66 @@
  * @brief Just a simple test application
  */
 #include "os.h"
+#include "test/test.h"
+#include "../trace.h"
+#include "syscall.h"
+
 
 DeclareTask(H1);
 DeclareTask(H2);
 DeclareTask(H3);
 
-int a;
+
+void test(void) {
+	test_start();
+	StartOS(0);
+}
+
+// new syscall to trigger interrupt using local APIC
+// for now, any function can be called using syscall(),
+// so it is easy to add this for this test.
+noinline void __OS_trigger_syscall(uint8_t irq) {
+	Machine::trigger_interrupt(irq);
+}
+
+static int cycle_count;
+
 
 TASK(H1) {
+	Trace('1');
 	TerminateTask();
 }
 
 TASK(H2) {
+	Trace('2');
+    arch::syscall(__OS_trigger_syscall, 37, true);
+	Trace('_');
 	TerminateTask();
 }
 
 TASK(H3) {
+	Trace('3');
 	ChainTask(H1);
 }
 
 ISR2(ISR1) {
+	Trace('{');
 	ActivateTask(H3);
+	Trace('}');
+}
+
+
+
+
+
+PreIdleHook() {
+	/* The testcase has finished, check the output */
+	cycle_count++;
+
+	if (cycle_count > 3) {
+		test_start_check();
+		TraceAssert((char *)"2{}_312{}_312{}_31");
+		ShutdownMachine();
+	}
 }
 
