c11392b2521ac4dce7fa8222f12fdb6f10e41543
Florian Lukas <florian.lukas@gmail.com>
2014-02-04 13:26:57 +0100

    fail: allow testing of generated apps
    FAIL* targets will be generated if FAIL option is given in
    COREDOS_BINARY macro.
    
    Change-Id: I864d0719dbcc6904b73fcb7c0b866f83910f40d0

--- toolchain/app.cmake
+++ toolchain/app.cmake
@@ -1,16 +1,7 @@
 include(CMakeParseArguments)
 include(rtsc)
 
-
-MACRO(COREDOS_BINARY)
-  set(options TEST_ISO)
-  set(oneValueArgs SYSTEM_XML NAME VERIFY)
-  set(multiValuedParameters SOURCES)
-  cmake_parse_arguments(COREDOS_BINARY "${options}" "${oneValueArgs}" "${multiValuedParameters}" ${ARGN} )
-  set(COREDOS_BINARY_SOURCES "${COREDOS_BINARY_UNPARSED_ARGUMENTS};${COREDOS_BINARY_SOURCES}")
-  SET(COREDOS_SYSTEM_XML "${CMAKE_CURRENT_SOURCE_DIR}/${COREDOS_BINARY_SYSTEM_XML}")
-  SET(NAME "${COREDOS_BINARY_NAME}")
-
+MACRO(COREDOS_BINARY_EXECUTABLE NAME SOURCES SYSTEM_XML VERIFY_SCRIPT DEFINITIONS)
   SET(COREDOS_ANNOTATE_SOURCE "${COREDOS_GENERATOR_DIR}/annotate/cored_annotate.cc")
   SET(COREDOS_ANNOTATE_OBJECT "${CMAKE_CURRENT_BINARY_DIR}/${NAME}_cored_annotate.ll")
 
@@ -19,7 +10,7 @@ MACRO(COREDOS_BINARY)
     COMMAND ${CLANGPP_BINARY} -S -emit-llvm
     -I${COREDOS_GENERATOR_DIR}/annotate/
     ${INCLUDEDIRS_FLAGS}
-    -Wno-return-type 
+    -Wno-return-type
     -m32 -std=c++11
     ${COREDOS_ANNOTATE_SOURCE} -o ${COREDOS_ANNOTATE_OBJECT}
     MAIN_DEPENDENCY ${COREDOS_ANNOTATE_SOURCE}
@@ -38,22 +29,22 @@ MACRO(COREDOS_BINARY)
   set(COREDOS_BINARY_LLVM_BYTECODE "")
   set(tmp "")
 
-  # First we have to compile all source files with clang
-  foreach(src ${COREDOS_BINARY_SOURCES})
-	set(llvm_bytecode "${CMAKE_CURRENT_BINARY_DIR}/${src}.ll")
-	add_custom_command(OUTPUT ${llvm_bytecode}
-	  COMMAND ${CLANGPP_BINARY} -S -emit-llvm -O0 
-      -m32 -std=c++11
-      ${INCLUDEDIRS_FLAGS}
-	  ${CMAKE_CURRENT_SOURCE_DIR}/${src} -o ${llvm_bytecode}
-	  MAIN_DEPENDENCY ${CMAKE_CURRENT_SOURCE_DIR}/${src}
-	  DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${src}
+  set(DEFINITON_FLAGS ";")
+  foreach(DEF ${DEFINITIONS})
+    set(DEFINITON_FLAGS ${DEFINITON_FLAGS} -D${DEF})
+  endforeach()
 
+  # First we have to compile all source files with clang
+  foreach(src ${SOURCES})
+    set(llvm_bytecode "${CMAKE_CURRENT_BINARY_DIR}/${src}.ll")
+    add_custom_command(OUTPUT ${llvm_bytecode}
+      COMMAND ${CLANGPP_BINARY} -S -emit-llvm -O0 -m32 -std=c++11 ${DEFINITON_FLAGS} ${INCLUDEDIRS_FLAGS} ${CMAKE_CURRENT_SOURCE_DIR}/${src} -o ${llvm_bytecode}
+      MAIN_DEPENDENCY ${src}
+      DEPENDS ${src}
       COMMENT "[${PROJECT_NAME}/${NAME}] Compiling application ${NAME}/${src} with clang")
 
-
-	list(APPEND COREDOS_BINARY_LLVM_BYTECODE ${llvm_bytecode})
-	list(APPEND tmp ${CMAKE_CURRENT_SOURCE_DIR}/${src})
+    list(APPEND COREDOS_BINARY_LLVM_BYTECODE ${llvm_bytecode})
+    list(APPEND tmp ${src})
   endforeach(src)
 
   SET(COREDOS_BINARY_SOURCES ${tmp})
@@ -61,25 +52,25 @@ MACRO(COREDOS_BINARY)
 
   # Use RTSC to analyze and merge the source system bytecode
   add_custom_command(OUTPUT "${COREDOS_SOURCE_SYSTEM}"
-	DEPENDS ${COREDOS_ANNOTATE_OBJECT}
+  DEPENDS ${COREDOS_ANNOTATE_OBJECT}
             ${COREDOS_BINARY_LLVM_BYTECODE}
             ${CMAKE_CURRENT_SOURCE_DIR}/*.xml
-	COMMAND ${EAG_BINARY} -data-deps=explicit -verify
-	   -sysann=${COREDOS_ANNOTATE_OBJECT}
-	   -sourcesystem=${CMAKE_CURRENT_SOURCE_DIR}/${COREDOS_BINARY_SYSTEM_XML}
-	   -out=${CMAKE_CURRENT_BINARY_DIR}
-	   -analyze-tasks -dump-source-system
+  COMMAND ${EAG_BINARY} -data-deps=explicit -verify
+     -sysann=${COREDOS_ANNOTATE_OBJECT}
+     -sourcesystem=${SYSTEM_XML}
+     -out=${CMAKE_CURRENT_BINARY_DIR}
+     -analyze-tasks -dump-source-system
        -dump-graphs
-	   ${COREDOS_BINARY_LLVM_BYTECODE}
+     ${COREDOS_BINARY_LLVM_BYTECODE}
     COMMAND
-       mv ${BDIR}/source_system.ll ${COREDOS_SOURCE_SYSTEM}
+      ${CMAKE_COMMAND} -E rename ${BDIR}/source_system.ll ${COREDOS_SOURCE_SYSTEM}
     COMMAND
-       mv ${BDIR}/rtsc_analyze.xml ${COREDOS_RTSC_ANALYZE_XML}
+       ${CMAKE_COMMAND} -E rename ${BDIR}/rtsc_analyze.xml ${COREDOS_RTSC_ANALYZE_XML}
     COMMENT "[${PROJECT_NAME}/${NAME}] Analyzing application with RTSC")
 
   # Add Target for the analysis step
-  add_custom_target(${COREDOS_BINARY_NAME}-rtsc-analyze
-	DEPENDS ${COREDOS_SOURCE_SYSTEM})
+  add_custom_target(${NAME}-rtsc-analyze
+  DEPENDS ${COREDOS_SOURCE_SYSTEM})
 
 
   # All python source files are a dependency
@@ -87,12 +78,11 @@ MACRO(COREDOS_BINARY)
   file(GLOB_RECURSE PYTHON_SOURCE "${COREDOS_GENERATOR_DIR}/*.py")
   file(GLOB_RECURSE OS_TEMPLATES "${PROJECT_SOURCE_DIR}/os/*.in")
   set(LINKER_TEMPLATE ${PROJECT_SOURCE_DIR}/arch/i386/linker.ld.in)
-  set(COREDOS_VERIFY_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/${COREDOS_BINARY_VERIFY}")
-  if(EXISTS "${COREDOS_VERIFY_SCRIPT}")
-    if (IS_DIRECTORY "${COREDOS_VERIFY_SCRIPT}")
+  if(EXISTS "${VERIFY_SCRIPT}")
+    if (IS_DIRECTORY "${VERIFY_SCRIPT}")
       SET(COREDOS_VERIFY_SCRIPT "")
     else()
-      SET(COREDOS_GENERATOR_ARGS ${COREDOS_GENERATOR_ARGS} --verify ${COREDOS_VERIFY_SCRIPT})
+      SET(COREDOS_GENERATOR_ARGS ${COREDOS_GENERATOR_ARGS} --verify ${VERIFY_SCRIPT})
     endif()
   else()
     SET(COREDOS_VERIFY_SCRIPT "")
@@ -100,25 +90,24 @@ MACRO(COREDOS_BINARY)
 
   # Generating COREDOS System
   add_custom_command(OUTPUT "${COREDOS_GENERATED_SOURCE}" ${COREDOS_GENERATED_LINKER}
-	DEPENDS ${PYTHON_SOURCE} "${COREDOS_SYSTEM_XML}" "${COREDOS_SOURCE_SYSTEM}"
+  DEPENDS ${PYTHON_SOURCE} "${SYSTEM_XML}" "${COREDOS_SOURCE_SYSTEM}"
             "${LLVM_NM_BINARY}" ${COREDOS_VERIFY_SCRIPT} ${OS_TEMPLATES} ${LINKER_TEMPLATE}
     COMMAND ${COREDOS_GENERATOR}
-	   --system-xml "${COREDOS_SYSTEM_XML}"
+     --system-xml "${SYSTEM_XML}"
        --rtsc-analyze-xml "${COREDOS_RTSC_ANALYZE_XML}"
-	   --prefix ${BDIR}
+     --prefix ${BDIR}
        --name ${NAME}
        --template-base ${PROJECT_SOURCE_DIR}
        ${COREDOS_GENERATOR_ARGS}
-	   -vv
-	COMMENT "[${PROJECT_NAME}/${NAME}] Generating COREDOS source code"
-	)
+     -vv
+  COMMENT "[${PROJECT_NAME}/${NAME}] Generating COREDOS source code"
+  )
 
-  add_custom_target(${COREDOS_BINARY_NAME}-generate
-	DEPENDS "${COREDOS_GENERATED_SOURCE}")
+  add_custom_target(${NAME}-generate DEPENDS "${COREDOS_GENERATED_SOURCE}")
 
-  add_custom_target(${COREDOS_BINARY_NAME}-clean
-	COMMAND rm -f ${CMAKE_CURRENT_BINARY_DIR}/${NAME}* ${COREDOS_BINARY_LLVM_BYTECODE}
-    )
+  add_custom_target(${NAME}-clean
+    COMMAND ${CMAKE_COMMAND} -E remove -f ${CMAKE_CURRENT_BINARY_DIR}/${NAME}* ${COREDOS_BINARY_LLVM_BYTECODE}
+  )
 
 
   # Since COREDOS_SOURCE_SYSTEM end in .ll the add_executable would
@@ -134,7 +123,27 @@ MACRO(COREDOS_BINARY)
   # Compile the coredos system
   include_directories(${RTSC_SOURCE_DIR}/data/SystemSupport/CoReD/include/)
   coredos_executable(${NAME} EXCLUDE_FROM_ALL
-    ${COREDOS_SOURCE_SYSTEM} ${COREDOS_GENERATED_SOURCE})
+    ${COREDOS_SOURCE_SYSTEM} ${COREDOS_GENERATED_SOURCE} LINKER_SCRIPT ${COREDOS_GENERATED_LINKER} DEFINITIONS ${DEFINITIONS})
+
+ENDMACRO()
+
+MACRO(COREDOS_BINARY)
+  set(options TEST_ISO FAIL)
+  set(oneValueArgs SYSTEM_XML NAME VERIFY)
+  set(multiValuedParameters SOURCES)
+  cmake_parse_arguments(COREDOS_BINARY "${options}" "${oneValueArgs}" "${multiValuedParameters}" ${ARGN} )
+  set(COREDOS_BINARY_SOURCES "${COREDOS_BINARY_UNPARSED_ARGUMENTS};${COREDOS_BINARY_SOURCES}")
+  SET(COREDOS_SYSTEM_XML "${CMAKE_CURRENT_SOURCE_DIR}/${COREDOS_BINARY_SYSTEM_XML}")
+  set(COREDOS_VERIFY_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/${COREDOS_BINARY_VERIFY}")
+  SET(NAME "${COREDOS_BINARY_NAME}")
+
+  COREDOS_BINARY_EXECUTABLE(${NAME} "${COREDOS_BINARY_SOURCES}" ${COREDOS_SYSTEM_XML} ${COREDOS_VERIFY_SCRIPT} "${DEFINITIONS}")
+
+  if(${COREDOS_BINARY_FAIL} STREQUAL "TRUE")
+    COREDOS_BINARY_EXECUTABLE(fail-${NAME} "${COREDOS_BINARY_SOURCES}" ${COREDOS_SYSTEM_XML} ${COREDOS_VERIFY_SCRIPT} "FAIL=1")
+
+    fail_test(${NAME} fail-${NAME})
+  endif()
 
   if(${COREDOS_BINARY_TEST_ISO} STREQUAL "TRUE")
     coredos_test_iso_image(test-${NAME} ${NAME} "${PROJECT_BINARY_DIR}/${NAME}.iso")
