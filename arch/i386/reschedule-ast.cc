5475b6267c0c04c127d4617c337e8ee6cbefcf3a
Florian Lukas <florian.lukas@gmail.com>
2014-05-05 13:14:53 +0200

    arch/i386: reduce overhead of reschedule AST
    Stack context used for interrupt return from reschedule AST is now
    stored in ROM, being more robust and efficient.
    
    Change-Id: I684c9badcfc210dca00432981760d25f5d2fcbae

--- arch/i386/reschedule-ast.cc
+++ arch/i386/reschedule-ast.cc
@@ -12,24 +12,17 @@ extern "C" uint8_t _estack_os;
 
 namespace arch {
 
-ISR(IRQ_RESCHEDULE) {
+static const uint32_t iret_schedule[]  = {
+	(uint32_t) __OS_ASTSchedule,
+	GDT::USER_CODE_SEGMENT | 0x3,
+	0x3200,
+	(uint32_t) &(_estack_os) - 2048,
+	GDT::USER_DATA_SEGMENT | 0x3
+};
+
+IRQ_HANDLER(IRQ_RESCHEDULE) {
 	// block ISR2s by raising APIC task priority
 	LAPIC::set_task_prio(128);
-
-	// set userspace segment selectors
-	// TODO: maybe not neccessary?
-	Machine::set_data_segment(GDT::USER_DATA_SEGMENT | 0x3);
-
-	// push the syscall stack address/segment
-	Machine::push(GDT::USER_DATA_SEGMENT | 0x3); // push stack segment, DPL3
-	Machine::push((uint32_t) &(_estack_os) - 2048); // push kernel stack pointer
-
-	// push flags, IO privilege level 3
-	Machine::push(cpu->eflags | 0x3000);
-
-	// push syscall function pointer/segment
-	Machine::push(GDT::USER_CODE_SEGMENT | 0x3); // push code segment, DPL3
-	Machine::push((uint32_t) __OS_ASTSchedule); // push address of scheduler function
 
 	// change to OS page directory
 	PageDirectory::enable(pagedir_os);
@@ -41,7 +34,7 @@ ISR(IRQ_RESCHEDULE) {
 	LAPIC::send_eoi();
 
 	// return from interrupt and proceed with syscall in ring 3
-	// TODO: optimization: put all values for iret in text segment?
+	asm volatile("mov %0, %%esp" :: "i"(&iret_schedule));
 	Machine::return_from_interrupt();
 }
 
