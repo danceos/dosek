51a526ab36e5b77ff24319a0efd232617457c1a8
Florian Lukas <florian.lukas@gmail.com>
2014-05-07 13:45:10 +0200

    arch/i386: force LAPIC inlining in irqs
    Change-Id: Ic53a5612c75a4563522dcb004310a055649e2c06

--- arch/i386/lapic.h
+++ arch/i386/lapic.h
@@ -9,6 +9,7 @@
 
 #include "stdint.h"
 #include "ioport.h"
+#include "os/util/inline.h"
 
 namespace arch {
 
@@ -27,11 +28,11 @@ class LAPIC {
 		ICR_HI = BASE + 0x0310, //!< interrupt control register, bits 32-63
 	};
 
-	static inline void write_reg(lapic_reg reg, uint32_t val) {
+	static forceinline void write_reg(lapic_reg reg, uint32_t val) {
 		*((volatile uint32_t*) reg) = val;
 	}
 
-	static inline uint32_t read_reg(lapic_reg reg) {
+	static forceinline uint32_t read_reg(lapic_reg reg) {
 		return *((volatile uint32_t*) reg);
 	}
 
@@ -40,29 +41,29 @@ public:
 	static void init();
 
 	/** \brief Enable local APIC */
-	static inline void enable() {
+	static forceinline void enable() {
 		uint32_t val = read_reg(SINT);
 		val |= 0x100; // enable APIC bit
 		write_reg(SINT, val);
 	}
 
 	/** \brief Send end-of-interrupt signal */
-	static inline void send_eoi() {
+	static forceinline void send_eoi() {
 		write_reg(EOI, 0);
 	}
 
 	/** \brief Return local APIC ID */
-	static inline uint8_t get_id() {
+	static forceinline uint8_t get_id() {
 		return read_reg(ID);
 	}
 
 	/** \brief Return interrupt request register */
-	static inline uint32_t get_irr(uint8_t offset) {
+	static forceinline uint32_t get_irr(uint8_t offset) {
 		return read_reg((lapic_reg) (IRR + (offset)*0x10));
 	}
 
 	/** \brief Return interrupt service register */
-	static inline uint32_t get_isr(uint8_t offset) {
+	static forceinline uint32_t get_isr(uint8_t offset) {
 		return read_reg((lapic_reg) (ISR + (offset)*0x10));
 	}
 
@@ -70,22 +71,22 @@ public:
 	 *
 	 * This will block all IRQ vectors lower than the given priority.
 	 */
-	static inline void set_task_prio(uint8_t prio) {
+	static forceinline void set_task_prio(uint8_t prio) {
 		write_reg(TPR, prio);
 	}
 
 	/** \brief Return task priority */
-	static inline uint8_t get_task_prio() {
+	static forceinline uint8_t get_task_prio() {
 		return static_cast<uint8_t>(read_reg(TPR));
 	}
 
 	/** \brief Return processor interrupt priority */
-	static inline uint8_t get_processor_prio() {
+	static forceinline uint8_t get_processor_prio() {
 		return static_cast<uint8_t>(read_reg(PPR));
 	}
 
 	/** \brief Trigger interrupt vector */
-	static inline void trigger(uint8_t vector) {
+	static forceinline void trigger(uint8_t vector) {
 		// write interrupt vector
 		// fixed delivery mode
 		// physical destination mode
@@ -95,7 +96,7 @@ public:
 	}
 
 	/** \brief Trigger NMI */
-	static inline void trigger_NMI() {
+	static forceinline void trigger_NMI() {
 		// NMI delivery mode
 		// physical destination mode
 		// assert level
