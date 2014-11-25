379e53a3930a29f0e8f8770f8416a90b552e2871
Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
2014-11-25 13:31:21 +0100

    ARM: .data section into ROM
    The initialized data section is put right after the text section and is
    copied during the boot process into the right place. This allows
    multiple runs of the same application with correctly initialized .data
    section.
    
    Change-Id: I49b6043aa537dff9d312edd88898e28887a250e5

--- arch/arm/constructors.cc
+++ arch/arm/constructors.cc
@@ -12,14 +12,29 @@ extern "C" {
     //! Address of the last global constructor (Defined by the linker script)
     extern void (*__CTORS_END)(void);
 
+	extern char _erom, _data, _edata, _bstart, _bend;
+
     void run_constructors(void) {
+		char *src = &_erom;
+		volatile char *dst = &_data;
+
+		/* ROM has data at end of text; copy it. */
+		while (dst < &_edata) {
+			*dst++ = *src++;
+		}
+
+		/* Zero bss */
+		for (dst = &_bstart; dst< &_bend; dst++) {
+			*dst = 0;
+		}
+
+
         //! Call constructors of all global object instances.
         //! @note Ensure that your linker script places
         //!       all `CTORS` between `__CTORS_START` and
         //! `__CTORS_END`
-        for( void (** volatile ctor)() = &__CTORS_START; ctor != &__CTORS_END; ++ctor )
-        {
+        for( void (** volatile ctor)() = &__CTORS_START; ctor != &__CTORS_END; ++ctor ) {
             (*ctor)();
-    }
-        }
+		}
+	}
 }
