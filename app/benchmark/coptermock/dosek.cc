danceos/dosek

commit 243a06f0c1c3a0b050fc46afbc4690611c554f0f

osek-v: possible base prescaling
After removing the static alarms from the system, we can optimize the
timer irq frequency. We choose the GCD resolution of all dynamic
counters and let the timer interrupt with a lower frequency. We're only
allowed to do this optimization, if the underlying count has a lower
resolution.

Change-Id: Ic39c8f6db142baa97a7f50b19c23a7a1fe601eaa

app/benchmark/coptermock/system.oil
@@ -151,7 +151,7 @@ CPU CopterMock {
     };
 
     ALARM CopterControlWatchdogAlarm {
-        COUNTER = C1;
+        COUNTER = C100;
         ACTION = ACTIVATETASK {
             TASK = CopterControlWatchdogTask;
         };
@@ -160,6 +160,12 @@ CPU CopterMock {
     COUNTER C1 {
         MAXALLOWEDVALUE = 50000;
         TICKSPERBASE = 1;
+        MINCYCLE = 1;
+    };
+
+    COUNTER C100 {
+        MAXALLOWEDVALUE = 20;
+        TICKSPERBASE = 10;
         MINCYCLE = 1;
     };
 

arch/osek-v/timer.cc
@@ -5,7 +5,10 @@
 
 namespace arch {
 
-void Timer::init() {
+unsigned Timer::interval;
+
+void Timer::init(unsigned interval_ms) {
+	interval = interval_ms;
 	reload();
     // Enable the timer interrupt
 	Machine::set_csr_bit(mie, MIP_MTIP);
@@ -13,7 +16,7 @@ void Timer::init() {
 
 void Timer::reload() {
 	uintptr_t time = Machine::read_csr(mtime);
-	Machine::write_csr(mtimecmp, time + 50);
+	Machine::write_csr(mtimecmp, time + 64 * interval);
 }
 
 void Timer::tick() {

arch/osek-v/timer.h
@@ -3,9 +3,11 @@
 
 namespace arch {
   struct Timer {
-	  static void init();
+	  static void init(unsigned interval_ms = 1);
 	  static void reload();
 	  static void tick();
+
+	  static unsigned interval;
   };
 }
 

generator/coder/os_generic.py
@@ -104,9 +104,10 @@ class GenericOS(BaseCoder):
 
             impl = self.generate_counter(ctr)
 
-            # Add and save counter Implementation
-            self.generator.source_file.data_manager.add(impl, namespace = ("os",))
-            ctr.impl = impl
+            if impl:
+                # Add and save counter Implementation
+                self.generator.source_file.data_manager.add(impl, namespace = ("os",))
+                ctr.impl = impl
 
         alarm_id = 1
         for alarm in self.generator.system_graph.alarms:
@@ -118,11 +119,11 @@ class GenericOS(BaseCoder):
             alarm_id += 1
 
             impl = self.generate_alarm(alarm, alarm.conf.counter, task)
-
-            # Add and save alarm implementation
-            self.generator.source_file.data_manager.add(impl, namespace = ("os",))
-            alarm.impl.name = impl.name
-            alarm.impl.alarm_desc = impl
+            if impl:
+                # Add and save alarm implementation
+                self.generator.source_file.data_manager.add(impl, namespace = ("os",))
+                alarm.impl.name = impl.name
+                alarm.impl.alarm_desc = impl
 
     def generate_system_code(self):
         pass

generator/coder/os_wired.py
@@ -140,6 +140,35 @@ class WiredOS(GenericOS):
     def system_leave_hook(self, abb, hook):
         self.callback_in_valid_passes("system_leave_hook", abb, hook)
 
+
+    def generate_counter(self, ctr):
+        static_alarm_pass = self.system_graph.get_pass("StaticAlarms")
+        if static_alarm_pass.is_static(ctr):
+            return None
+        return DataObject("UnencodedCounter",
+                             "OS_%s_counter" % (ctr.name),
+                             "(%d, %d, %d)" % (ctr.conf.maxallowedvalue,
+                                               ctr.conf.ticksperbase,
+                                               ctr.conf.mincycle))
+
+    def generate_alarm(self, alarm, counter, task):
+        static_alarm_pass = self.system_graph.get_pass("StaticAlarms")
+        if static_alarm_pass.is_static(alarm):
+            return None
+
+        period = static_alarm_pass.dynamic_counter_prescaler
+
+        return DataObject("UnencodedAlarm<&%s>" % counter.impl.name,
+                           "OS_%s_alarm" %( alarm.conf.name),
+                           "(%s, %s, %d, %d, %d)" % \
+                          (task,
+                           str(alarm.conf.armed).lower(),
+                           (alarm.conf.reltime + period - 1) / period,
+                           (alarm.conf.cycletime + period - 1) / period,
+                           alarm.impl.alarm_id,
+                          ))
+
+
     def get_syscall_return_variable(self, Type, size = 2):
         """Returns a Variable, that is able to capture the return value of a
            system call.

generator/coder/syscall_wired.py
@@ -81,7 +81,8 @@ class WiredSystemCalls(BaseCoder):
         # Enable the timer, if the alarm subsystem is enabled
         if self.alarm_subsystem.subsystem_enabled:
             self.generator.source_file.include("timer.h")
-            self.call_function(kernelspace, "arch::Timer::init")
+            self.call_function(kernelspace, "arch::Timer::init", "void",
+                               [str(self.static_alarm_pass.dynamic_counter_prescaler)])
         StartOS = self.system_graph.get(GraphFunction, "StartOS")
         self.fsm_schedule(StartOS.entry_abb, None, kernelspace)
 
@@ -164,11 +165,21 @@ class WiredSystemCalls(BaseCoder):
 
     def SetRelAlarm(self, syscall, userspace, kernelspace):
         self.fsm_schedule(syscall, userspace, kernelspace)
+        prescaler = self.static_alarm_pass.dynamic_counter_prescaler
+        if prescaler != 1:
+            reltime = self.arch_rules.get_syscall_argument(kernelspace, 0)
+            cycle = self.arch_rules.get_syscall_argument(kernelspace, 1)
+            kernelspace.add(Statement("{var} = ({var} + {prescaler} - 1) / {prescaler}"\
+                                      .format(var=reltime.name, prescaler=prescaler)))
+            kernelspace.add(Statement("{var} = ({var} + {prescaler} - 1) / {prescaler}"\
+                                      .format(var=cycle.name, prescaler=prescaler)))
         FullSystemCalls.SetRelAlarm(self, syscall, userspace, kernelspace)
 
     def GetAlarm(self, syscall, userspace, kernelspace):
         self.fsm_schedule(syscall, userspace, kernelspace)
         FullSystemCalls.GetAlarm(self, syscall, userspace, kernelspace)
+        # FIXME: Should implement prescaler
+        assert self.static_alarm_pass.dynamic_counter_prescaler == 1, "Scaling of results not implemented"
 
     def CancelAlarm(self, syscall, userspace, kernelspace):
         self.fsm_schedule(syscall, userspace, kernelspace)

generator/transform/StaticAlarms.py
@@ -5,6 +5,7 @@ from generator.analysis.AtomicBasicBlock import E, S
 from generator.coder.elements import DataObject, Include
 from collections import namedtuple
 from fractions import gcd
+from functools import reduce
 import logging
 
 class StaticAlarms(Analysis):
@@ -29,7 +30,10 @@ class StaticAlarms(Analysis):
         for abb in self.system_graph.abbs:
             # All manipulated alarms and counters are non-static
             if abb.isA([S.SetRelAlarm, S.CancelAlarm]):
-                static_alarms[abb.arguments[0]] = False
+                alarm = abb.arguments[0]
+                static_alarms[alarm] = False
+                static_counters[alarm.conf.counter] = False
+
             if abb.isA([S.AdvanceCounter]):
                 static_counters[abb.arguments[0]] = False
 
@@ -65,9 +69,19 @@ class StaticAlarms(Analysis):
                 cycletime = alarm.conf.cycletime//self.base_period,
             )
 
+        # Among all dynamic counters, what is the base prescaler value
+        self.static_counters = [k for k,v in static_counters.items() if v is True]
+        dynamic_counters = [k for k,v in static_counters.items() if v is False]
+        if dynamic_counters:
+            self.dynamic_counter_prescaler = reduce(gcd, [c.conf.TICKSPERBASE for c in dynamic_counters])
+        else:
+            self.dynamic_counter_prescaler = 1
+        logging.info("  Dynamic Counters: IRQ every %d ticks", self.dynamic_counter_prescaler)
+
+
     # Accessors for the analysis results
-    def is_static(self, alarm):
-        return alarm in self.static_alarms
+    def is_static(self, alarm_or_counter):
+        return alarm_or_counter in self.static_alarms or alarm_or_counter in self.static_counters
 
     def is_useless(self, alarm):
         return alarm in self.useless_alarms

commit 04fef30707c805d4d69ca4603dafa0439b1dc501

coptermock: fix timing for osek-v
The maxallowedvalue of the watchdog timer was too small for the actual
usage of the timer. This regression was introduced in 243a06f0. So it
has no influence of already published research, since it only appears in
the osek-v branch.

Change-Id: I074d5b757e03bf8632e838a9f5cbd9e25ba2182e

app/benchmark/coptermock/dosek.cc
@@ -129,7 +129,7 @@ TASK(CopterControlTask) {
 	if (round < 5) {
 		CancelAlarm(CopterControlWatchdogAlarm);
 		test_trace(0x94);
-		SetRelAlarm(CopterControlWatchdogAlarm, 100, 100);
+		SetRelAlarm(CopterControlWatchdogAlarm, 110, 100);
 	}
 	test_trace(0x95);
 
@@ -137,9 +137,14 @@ TASK(CopterControlTask) {
 }
 
 ISR2(MavlinkRecvHandler) {
+#ifndef CONFIG_ARCH_OSEK_V
 	test_trace(0xA1);
+#endif
+
 	ActivateTask(CopterControlTask);
+#ifndef CONFIG_ARCH_OSEK_V
 	test_trace(0xA2);
+#endif
 }
 
 TASK(CopterControlWatchdogTask) {

app/benchmark/coptermock/system.oil
@@ -164,7 +164,7 @@ CPU CopterMock {
     };
 
     COUNTER C100 {
-        MAXALLOWEDVALUE = 20;
+        MAXALLOWEDVALUE = 1000;
         TICKSPERBASE = 10;
         MINCYCLE = 1;
     };